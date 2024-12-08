6f7f7585a47099583193441d3f64bd18757b7413
TomekWojdyla <tw.kota@gmail.com>
2024-12-08 19:35:20 +0100

    Settery

--- Animals.cs
+++ Animals.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 internal class Animals
 {
+    //Pola Prywatne
+    private string _description = "Unknown"; //konwencja nazywania pól prywatnych _camelCase
+
     //Właściwości + gettery/settery
-    public required string Description { get; init; }
+    public required string Description
+    {
+        get
+        {
+            return _description;
+        }
+        init
+        {
+            string validatedDescription = value.Trim(); //Ucina białe znaki na początku i na końcu
+            if (validatedDescription == "" || validatedDescription == null) //Wyłapanie sytuacji w której z nazwy nic nie zostało po Trim()
+            {
+                _description = "Unknown";
+            }
+            else
+            {
+                if (validatedDescription.Length < 3)
+                {
+                    validatedDescription = validatedDescription.PadRight(3, '#'); //Uzyskanie nazwy na min 3 znaki
+                }
+                else if (validatedDescription.Length > 15)
+                {
+                    validatedDescription = validatedDescription.Substring(0, 15).TrimEnd(); //ucięcie stringa do max 25 znaków + ucięcie białych znaków z końca
+                    if (validatedDescription.Length < 3)
+                    {
+                        validatedDescription = validatedDescription.PadRight(3, '#'); //Uzyskanie nazwy na min 3 znaki
+                    }
+                }
+                if (char.IsLower(validatedDescription[0]))
+                {
+                    validatedDescription = char.ToUpper(validatedDescription[0]) + validatedDescription.Substring(1); //Ustawienie pierwszej litery na wielka
+                }
+                _description = validatedDescription;
+            }
+        }
+    }
     public uint Size { get; set; } = 3;
 
     public string Info
--- Creature.cs
+++ Creature.cs
@@ -8,14 +8,73 @@ namespace Simulator;
 
 internal class Creature
 {
+    //Pola Prywatne
+    private string _name = "Unknown"; //konwencja nazywania pól prywatnych _camelCase
+    private int _level;
+
     //Właściwości + gettery/settery
-    public string Name { get; set; }
-    public int Level { get; set; }
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        init
+        {
+            string validatedName = value.Trim(); //Ucina białe znaki na początku i na końcu
+            if (validatedName == "" || validatedName == null) //Wyłapanie sytuacji w której z nazwy nic nie zostało po Trim()
+            {
+                _name = "Unknown";
+            }
+            else
+            {
+                if (validatedName.Length < 3)
+                {
+                    validatedName = validatedName.PadRight(3, '#'); //Uzyskanie nazwy na min 3 znaki
+                }
+                else if (validatedName.Length > 25)
+                {
+                    validatedName = validatedName.Substring(0, 25).TrimEnd(); //ucięcie stringa do max 25 znaków + ucięcie białych znaków z końca
+                    if (validatedName.Length < 3)
+                    {
+                        validatedName = validatedName.PadRight(3, '#'); //Uzyskanie nazwy na min 3 znaki
+                    }
+                }
+                if (char.IsLower(validatedName[0]))
+                {
+                    validatedName = char.ToUpper(validatedName[0]) + validatedName.Substring(1); //Ustawienie pierwszej litery na wielka
+                }
+                _name = validatedName;
+            }
+        }
+    }
+    public int Level
+    {
+        get
+        {
+            return _level;
+        }
+        init
+        {
+            if (value < 1)
+            {
+                _level = 1;
+            }
+            else if (value > 10)
+            {
+                _level = 10;
+            }
+            else
+            {
+                _level = value;
+            }
+        }
+    }
 
     //Konstruktor parametryczny
     public Creature(string name, int level = 1)
     {
-        Name = name; 
+        Name = name;
         Level = level;
     }
 
@@ -33,5 +92,13 @@ internal class Creature
     public string Info
     {
         get { return $"{Name} [{Level}]"; }
+    }
+
+    public void Upgrade()
+    {
+        if (_level < 10) //Sprawdzenie, że nie ma levelu 10 przed podniesieniem o 1
+        {
+            _level++;
+        }
     }
 }
