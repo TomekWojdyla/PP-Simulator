e7b4e4af4f1ad8999ca82c7b099a273ec9b65604
TomekWojdyla <tw.kota@gmail.com>
2024-12-08 19:03:52 +0100

    Stwory

---
+++ Animals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Animals
+{
+    //Właściwości + gettery/settery
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+    public string Info
+    {
+        get { return $"{Description} <{Size}>"; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,15 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+
+        //test pkt.2
+        //Creature a = new("Shrek", 8);
+        //a.SayHi();
+        //Console.WriteLine(a.Info);
+        //Creature b = new("Fiona");
+        //b.SayHi();
+        //Console.WriteLine(b.Info);
+        //Animals z = new() { Description = "Wombats" };
+        //Console.WriteLine(z.Info);
     }
 }
